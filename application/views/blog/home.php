cf3105a96a227aeb41ce8d809b2ebd8fee3fcdc5
Ralph Saunders <e@ralphsaunders.co.uk>
2012-06-14 18:35:39 +0100

    Added some styles

--- application/views/blog/home.php
+++ application/views/blog/home.php
@@ -1,23 +1,25 @@
-Homepage
-
 <?php
 $this->load->helper( 'url' );
 $this->load->helper( 'naming' );
 ?>
 
-<pre>
-<?php var_dump( $posts ); ?>
-</pre>
+<section class="pl-posts">
 
-<ul>
-<?php if( isset( $posts ) ) : foreach( $posts as $post ): ?>
-    <li>
-        <a href="<?php echo $post['relative_path'] . $post['name']; ?>"
-           title="<?php echo title_case( $post['name'] ); ?>"
-        ><?php echo title_case( $post['name'] ); ?></a>
-    </li>
-<?php endforeach; endif; ?>
-</ul>
+    <h1>articles</h1>
+
+    <ul class="pl-post-list">
+    <?php if( isset( $posts ) ) : foreach( $posts as $post ): ?>
+        <li>
+            <h3>
+                <a href="<?php echo $post['relative_path'] . $post['name']; ?>"
+                   title="<?php echo title_case( $post['name'] ); ?>"
+                ><?php echo title_case( $post['name'] ); ?></a>
+            </h3>
+        </li>
+    <?php endforeach; endif; ?>
+    </ul>
+
+</section>
 
 <?php if( ! isset( $posts ) || empty( $posts ) ) : ?>
 You have no posts!
--- application/views/includes/header.php
+++ application/views/includes/header.php
@@ -4,5 +4,7 @@
         <title>
             <?php if( isset( $title ) ) { echo $title; } ?>
         </title>
+
+        <link href="assets/css/styles.css" type="text/css" rel="stylesheet">
     </head>
     <body>
